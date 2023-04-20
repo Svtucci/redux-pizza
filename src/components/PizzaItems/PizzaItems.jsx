bea404a6491aed2dbd5dc3c1d7912a31d0d43a04
Stephen Vertucci <steve.vertucci@gmail.com>
2023-04-20 19:18:48 -0400

    formatted page and added css

--- src/components/PizzaItems/PizzaItems.jsx
+++ src/components/PizzaItems/PizzaItems.jsx
@@ -5,28 +5,30 @@ import { useDispatch, useSelector } from 'react-redux';
 //TODO import mui 
 
 
-function PizzaItems( {pizza}) {
-const dispatch = useDispatch ();
-const cart = useSelector(store => store.cart);
+function PizzaItems({ pizza }) {
+    const dispatch = useDispatch();
+    const cart = useSelector(store => store.cart);
 
-const addPizzaToCart = () => {
-    console.log(product);
-}
+    const addPizzaToCart = () => {
+        console.log(product);
+    }
     //! DELETE request
     // handle add
     // handle remove
 
 
-
-
     //!What will display on the DOM
     return (
         <>
-        <h2>Pizza Items</h2>
+            
             <li>
-        {pizza.name}: {pizza.price}{''}
-        {pizza.image_path}
-        <button onClick={addPizzaToCart}> Add To Cart</button>
+                <h3>{pizza.name}:</h3> {''}
+                <br />
+                <img src={pizza.image_path} />
+                <br />
+                {pizza.price}
+                
+                <button onClick={addPizzaToCart}> Add To Cart</button>
 
 
             </li>
